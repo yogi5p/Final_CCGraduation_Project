713a1cf1276f26558992e27d24d1375e9e9f358b
Bob Townsend <bt.codes.it@gmail.com>
2018-05-01 15:27:21 -0700

    Re-scaffolded project to web-app

--- README.md
+++
@@ -1,1 +0,0 @@
-"# Final_CCGraduation_Project" 
--- fb.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  clientID: "431866953929221",
-  clientSecret: "272756f6aba8140d474662bd9c159057",
-  callbackUrl: "https://localhost:3030/login/facebook/callback"
-};
