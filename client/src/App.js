97e81e6f1621b9f80df5e86fb7f1cca320586bd8
Lyric Payton <lyricpayton@Lyrics-MacBook-Pro.local>
2018-05-07 10:57:12 -0700

    before updating changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,315 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import AppCss from "./App.css";
+import about from "./about.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <div id="header">
+          <a href="/" id="logo">
+            <img src="images/logo.gif" width="310" height="114" alt="" />
+          </a>
+          <ul class="navigation">
+            <li class="active">
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="petmart.html">PetMart</a>
+            </li>
+            <li>
+              <a href="about.js">About us</a>
+            </li>
+            <li>
+              <a href="about.js">Blog</a>
+            </li>
+            <li>
+              <a href="petguide.html">PetGuide</a>
+            </li>
+            <li>
+              <a href="contact.html">Contact us</a>
+            </li>
+          </ul>
+        </div>
+        <div id="body">
+          <div class="banner">&nbsp;</div>
+          <div id="content">
+            <div class="content">
+              <ul>
+                <li>
+                  <a href="#">
+                    <img
+                      src="images/puppy.jpg"
+                      width="114"
+                      height="160"
+                      alt=""
+                    />
+                  </a>
+                  <h2>What they need</h2>
+                  <p>
+                    Mirum est notare quam littera gothica, quam nunc putamus
+                    parum clara m, ant epo suerit li tterar.
+                    <a class="more" href="#">
+                      View all
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="images/cat.jpg" width="114" height="160" alt="" />
+                  </a>
+                  <h2>Something good</h2>
+                  <p>
+                    Gothica, quam nun c putamus parum claram, anteposuerit
+                    litterarum formas humani tatis per seacula.
+                    <a class="more" href="#">
+                      View all
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="images/koi.jpg" width="114" height="160" alt="" />
+                  </a>
+                  <h2>Kinds of Fish</h2>
+                  <p>
+                    Quam nunc putamus parum claram, antep osuerit litter arum
+                    formas humanitatis per seacula quarta.
+                    <a class="more" href="#">
+                      View all
+                    </a>
+                  </p>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="images/bird.jpg"
+                      width="114"
+                      height="160"
+                      alt=""
+                    />
+                  </a>
+                  <h2>Fun birds</h2>
+                  <p>
+                    Mirum est notare quam littera gothica , quam nunc putamus
+                    parum claram, anteposuerit.
+                    <a class="more" href="#">
+                      View all
+                    </a>
+                  </p>
+                </li>
+              </ul>
+            </div>
+            <div id="sidebar">
+              <div class="search">
+                <input type="text" name="s" value="Find" />
+                <button>&nbsp;</button>
+                <label for="articles">
+                  <input type="radio" id="articles" /> Articles
+                </label>
+                <label for="products">
+                  <input type="radio" id="products" checked /> PetMart Products
+                </label>
+              </div>
+              <div class="section">
+                <ul class="navigation">
+                  <li class="active">
+                    <a href="#">Shopping Guides</a>
+                  </li>
+                  <li>
+                    <a href="#">Discounted Items</a>
+                  </li>
+                </ul>
+                <div class="aside">
+                  <div>
+                    <div>
+                      <ul>
+                        <li>
+                          <a href="#">Pet Accesories </a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Bath Essentials</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Food</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Vitamins</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Needs</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Toy and Treats</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Supplies</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">Pet Emergency Kit</a>
+                          <a class="more" href="#">
+                            see all
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="featured">
+            <ul>
+              <li>
+                <a href="#">
+                  <img
+                    src="images/organic-and-chemical-free.jpg"
+                    width="300"
+                    height="90"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="images/good-food.jpg"
+                    width="300"
+                    height="90"
+                    alt=""
+                  />
+                </a>
+              </li>
+              <li class="last">
+                <a href="#">
+                  <img
+                    src="images/pet-grooming.jpg"
+                    width="300"
+                    height="90"
+                    alt=""
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="footer">
+          <div class="section">
+            <ul>
+              <li>
+                <img
+                  src="images/friendly-pets.jpg"
+                  width="240"
+                  height="186"
+                  alt=""
+                />
+                <h2>
+                  <a href="#">Friendly Pets</a>
+                </h2>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed
+                  diam nonummy nib.
+                  <a class="more" href="#">
+                    Read More
+                  </a>
+                </p>
+              </li>
+              <li>
+                <img
+                  src="images/pet-lover2.jpg"
+                  width="240"
+                  height="186"
+                  alt=""
+                />
+                <h2>
+                  <a href="#">How dangerous are they</a>
+                </h2>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, cons ectetuer adepis cing, sed
+                  diam euis.
+                  <a class="more" href="#">
+                    Read More
+                  </a>
+                </p>
+              </li>
+              <li>
+                <img
+                  src="images/healthy-dog.jpg"
+                  width="240"
+                  height="186"
+                  alt=""
+                />
+                <h2>
+                  <a href="#">Keep them healthy</a>
+                </h2>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed
+                  diam nonu mmy.
+                  <a class="more" href="#">
+                    Read More
+                  </a>
+                </p>
+              </li>
+              <li>
+                <h2>
+                  <a href="#">Love...love...love...pets</a>
+                </h2>
+                <p>
+                  {" "}
+                  Lorem ipsum dolor sit amet, consectetuer adepiscing elit, sed
+                  diameusim.
+                  <a class="more" href="#">
+                    Read More
+                  </a>
+                </p>
+                <img
+                  src="images/pet-lover.jpg"
+                  width="240"
+                  height="186"
+                  alt=""
+                />{" "}
+              </li>
+            </ul>
+          </div>
+          <div id="footnote">
+            <div class="section">
+              Copyright &copy; 2018
+              <a href="#">Waggs Space</a> All rights reserved
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "/Users/lyricpayton/Desktop/final_project/client/src/stylesheets/style.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
