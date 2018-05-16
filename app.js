4be7dc273789dcc12693bfec7821d92a1f8f2292
David Chatfield <chatfielddc2@cox.net>
2018-05-16 14:55:43 -0700

    removed unwanted items

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+var mongoose = require("mongoose");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var require = "./models/user";
