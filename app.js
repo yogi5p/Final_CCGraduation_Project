974d3677de9903a8c58f9a43057cab8cc3410f91
David Chatfield <chatfielddc2@cox.net>
2018-05-16 14:55:59 -0700

    and again

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var mongoose = require("mongoose");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var require = "./models/user";
