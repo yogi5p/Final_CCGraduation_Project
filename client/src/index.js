7d90b9c79893ceb5af1aadb7dcb636bfa041a2a3
Chelsea Sanders <you@example.com>
2018-05-18 12:03:30 -0700

    Fourth Commit(Installs and correction in index.js)

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import "C:/Users/rac/Downloads/Final_CCGraduation_Project/client/src/stylesheets/style.css";
-
 import "./stylesheets/style.css";
 
 import App from "./App";
