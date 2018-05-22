6e64460ef1f2b83b1594f5694ab35846b70e67c7
Chelsea Sanders <you@example.com>
2018-05-22 10:49:28 -0700

    Fifth commit(img,pet guide and html edit)

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import "./stylesheets/style.css";
+import "./stylesheet/style.css";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
