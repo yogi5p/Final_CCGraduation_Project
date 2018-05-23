4c40aa3205af52728e68ecedc7a04b3bdd95eb36
Chelsea Sanders <you@example.com>
2018-05-23 10:08:39 -0700

    Sixth commit(Petgallery, img, lightbox)

--- app.js
+++ app.js
@@ -30,9 +30,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", routes.index);
-app.post("/create", routes.create);
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
